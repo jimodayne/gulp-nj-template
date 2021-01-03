2ad3691983a8d0081d2a7f292650863e8faee423
Thinh Tran <thinhtd2@vng.com.vn>
2021-01-03 18:09:32 +0700

    feat: update browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 
 // Load plugins
-
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
@@ -9,45 +8,47 @@ const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const concat = require('gulp-concat');
 const clean = require('gulp-clean');
-const imagemin = require('gulp-imagemin');
+const nunjucks = require('gulp-nunjucks');
 const changed = require('gulp-changed');
-const browsersync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 
-// Clean assets
+const BASE_DIR = 'src/';
+const BASE_DEST = 'dest/';
 
-function clear() {
-    return src('./dest/*', {
+const filesPath = {
+    scss: BASE_DIR + 'scss/**/*.scss',
+    js: BASE_DIR + 'js/**/*.js',
+    html: BASE_DIR + 'template/*.{html,njk}',
+    img: BASE_DIR + 'images/**/*'
+};
+const destPath = {
+    scss: BASE_DEST + 'css/',
+    js: BASE_DEST + 'js/',
+    html: BASE_DEST,
+    img: BASE_DEST + 'images/'
+};
+
+function clearTask() {
+    return src('dest/*', {
         read: false
     }).pipe(clean());
 }
 
-function html() {}
-
-// JS function
-
-function js() {
-    const source = './src/js/*.js';
-
-    return src(source)
-        .pipe(changed(source))
+function jsTask() {
+    return src(filesPath.js)
+        .pipe(changed(filesPath.js))
         .pipe(concat('bundle.js'))
         .pipe(uglify())
-        .pipe(
-            rename({
-                extname: '.min.js'
-            })
-        )
-        .pipe(dest('./dest/js/'))
-        .pipe(browsersync.stream());
+        .pipe(dest(destPath.js));
 }
 
-// CSS function
+function imgTask() {
+    return src(filesPath.img).pipe(dest(destPath.img));
+}
 
-function css() {
-    const source = './src/css/*.scss';
-
-    return src(source)
-        .pipe(changed(source))
+function scssTask() {
+    return src(filesPath.scss)
+        .pipe(changed(filesPath.scss))
         .pipe(sass())
         .pipe(
             autoprefixer({
@@ -61,41 +62,31 @@ function css() {
             })
         )
         .pipe(cssnano())
-        .pipe(dest('./dest/css/'))
-        .pipe(browsersync.stream());
+        .pipe(dest(destPath.scss))
+        .pipe(browserSync.stream());
+}
+function htmlTask() {
+    return src(filesPath.html)
+        .pipe(nunjucks.compile())
+        .pipe(dest(destPath.html));
 }
 
-// Optimize images
-
-function img() {
-    return (
-        src('./src/img/*')
-            // .pipe(imagemin())
-            .pipe(dest('./dest/images'))
-    );
+function watchTask() {
+    watch(filesPath.scss, scssTask);
+    watch(filesPath.js, jsTask);
+    watch(filesPath.html, htmlTask).on('change', browserSync.reload);
 }
 
-// Watch files
-
-function watchFiles() {
-    watch('./src/scss/*', css);
-    watch('./src/js/*', js);
-    watch('./src/images/*', img);
-    watch('./src/template/*', html);
-}
-
-// BrowserSync
-
-function browserSync() {
-    browsersync.init({
+function browserSyncTask() {
+    browserSync.init({
         server: {
-            baseDir: './'
-        },
-        port: 3000
+            baseDir: './dest/'
+        }
     });
 }
 
-// Tasks to define the execution of the functions simultaneously or in series
-
-exports.watch = parallel(watchFiles, browserSync);
-exports.default = series(clear, parallel(js, css, img));
+exports.default = series(
+    clearTask,
+    parallel(scssTask, jsTask, htmlTask, imgTask),
+    parallel(browserSyncTask, watchTask)
+);
